c35ef277fabdba718a9a084753dbc4cd2237415b
Lucas Bernardes <lucascbernardes@live.com>
2016-08-27 23:30:05 -0300

    Feito split da msg

--- projeto1/HTTPServer.java
+++ projeto1/HTTPServer.java
@@ -4,30 +4,56 @@ import java.io.*;
 public class HTTPServer {
 
     public static void main(String[] args) throws IOException {
-        int port = 4444;
+
+        int port = Integer.parseInt(args[0]);
         ServerSocket server = new ServerSocket(port);
         System.err.println("Started server on port " + port);
         System.err.println("Waiting for connection...");
 
         Socket client = server.accept();
-        System.err.println("Accepted connection from client.");
+        System.err.println("Accepted connection from client " +
+            client.getInetAddress().toString());
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+        BufferedReader clientIn = new BufferedReader(
+            new InputStreamReader(client.getInputStream())
+        );
+        PrintWriter clientOut = new PrintWriter(client.getOutputStream(), true);
 
-        String s;
-        while ((s = input.readLine()) != null) {
-            System.err.println("Received: " + s);
-            output.println(s);
-
-            if (s.equals("bye")) {
+        String stringReceived;
+        String[] s;
+        while ((stringReceived = clientIn.readLine()) != null) {
+            System.err.println("Received: " + stringReceived);
+            // Quebra a mensagem recebida em partes e verifica o tipo da requisição HTTP
+            s = stringReceived.split(" ");
+            if (s[0].equals("GET")) {
+                clientOut.println("GET");
+                continue;
+            }
+            else if (s[0].equals("PUT")) {
+                clientOut.println("PUT");
+                continue;
+            }
+            else if (s[0].equals("HEAD")) {
+                clientOut.println("HEAD");
+                continue;
+            }
+            else if (s[0].equals("POST")) {
+                clientOut.println("POST");
+                continue;
+            }
+            else if (s[0].equals("DELETE")) {
+                clientOut.println("DELETE");
+                continue;
+            }
+            else {
+                clientOut.println("HTTP/1.1 400 Bad Request");
                 break;
             }
         }
 
-        System.err.println("Closing connection with client.");
-        output.close();
-        input.close();
+        System.err.println("Closed connection with client");
+        clientOut.close();
+        clientIn.close();
         client.close();
         server.close();
 
